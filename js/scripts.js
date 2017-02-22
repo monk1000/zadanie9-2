ef859f3ec3b32f3962cf8778541f76062eaf0d53
Tomasz Kacprzak <monkhtc@gmail.com>
2017-02-22 10:11:37 +0100

    pole trojkata

--- js/scripts.js
+++ js/scripts.js
@@ -6,3 +6,13 @@ alert('Witaj, ' + name);
 //zadanie 8.3
 // dodatkowe 2 komentarze
 console.log('Witaj, ' + name);
+
+
+//Triangle
+
+var a = prompt('Wpisz długość podstawy trójkąta w cm: ');
+	h = prompt('Wpisz wysokość trójkąta w cm: ');
+	triangleArea = a * h / 2;
+alert('Pole Twojego trójkąta wynosi ' + triangleArea);
+console.log('Pole trójkąta o podstawie a: ' + a + ' i wysokości h: ' + h + ' wynosi: ' + triangleArea);
+console.log('Wynik = ' + triangleArea)
