dc466c65199ad6d8c0c50d163d3d403712c25e89
Tomasz Kacprzak <monkhtc@gmail.com>
2017-02-28 09:41:10 +0100

    zadanie 8.6

--- js/scripts.js
+++ js/scripts.js
@@ -35,44 +35,44 @@
 
 //zadanie 8.6
 
-//var a = prompt('Podaj w cm długość podstawy Twojego trójkąta: ');
-//	h = prompt('Podaj w cm wysokość Twojego trójkąta: ');
-//	triangle0Area = getTriangleArea(a, h);
-//	triangle1Area = getTriangleArea(10, 32);
-//	triangle2Area = getTriangleArea(3, 342);
-//	triangle3Area = getTriangleArea(23, 352);
+var a = prompt('Podaj w cm długość podstawy Twojego trójkąta: ');
+	h = prompt('Podaj w cm wysokość Twojego trójkąta: ');
+	triangle0Area = getTriangleArea(a, h);
+	triangle1Area = getTriangleArea(10, 32);
+	triangle2Area = getTriangleArea(3, 342);
+	triangle3Area = getTriangleArea(23, 352);
 
-//function getTriangleArea(a, h) {
-//
-//		if (a <= 0 || h <= 0) {
-//			alert('Uknown Data');	
-//		} 
-//		
-//		return a * h / 2;
-//}
-//
-//getTriangleArea(triangle0Area);
-//console.log('Pole twojego trójkąta wynosi: ' + triangle0Area);
+	function getTriangleArea(a, h) {
 
-//getTriangleArea(triangle1Area);
-//console.log('Pole trójkąta o podstawie 10 i wysokosci 32 wynosi: ' + triangle1Area);
-//
-//getTriangleArea(triangle2Area);
-//console.log('Pole trójkąta o podstawie 3 i wysokosci 342 wynosi: ' + triangle2Area);
-//
-//getTriangleArea(triangle3Area);
-//console.log('Pole trójkąta o podstawie 23 i wysokosci 352 wynosi: ' + triangle3Area);
+		if (a <= 0 || h <= 0) {
+			alert('Uknown Data');	
+		} 
+		
+		return a * h / 2;
+}
+
+getTriangleArea(triangle0Area);
+console.log('Pole twojego trójkąta wynosi: ' + triangle0Area);
+
+getTriangleArea(triangle1Area);
+console.log('Pole trójkąta o podstawie 10 i wysokosci 32 wynosi: ' + triangle1Area);
+
+getTriangleArea(triangle2Area);
+console.log('Pole trójkąta o podstawie 3 i wysokosci 342 wynosi: ' + triangle2Area);
+
+getTriangleArea(triangle3Area);
+console.log('Pole trójkąta o podstawie 23 i wysokosci 352 wynosi: ' + triangle3Area);
 
 
 //zadanie 8.7
 
-var femaleNames = ['Asia', 'Kasia', 'Ola', 'Jola'];
-	maleNames = ['Piotrek', 'Marek', 'Arek', 'Jarek'];
-	allNames = femaleNames.concat(maleNames);
-	newName = 'Marian';
-
-	if (allNames.indexOf(newName) === -1) {
-	} 
-	allNames.push(newName);
-	
-console.log(allNames);
+//var femaleNames = ['Asia', 'Kasia', 'Ola', 'Jola'];
+//	maleNames = ['Piotrek', 'Marek', 'Arek', 'Jarek'];
+//	allNames = femaleNames.concat(maleNames);
+//	newName = 'Marian';
+//
+//	if (allNames.indexOf(newName) === -1) {
+//	} 
+//	allNames.push(newName);
+//	
+//console.log(allNames);
